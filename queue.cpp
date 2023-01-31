4ce6d55f9294a9a7d13fe5983eb26f5df3a90aef
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-01-31 21:10:48 +0530

    Queue implementation added

---
+++ queue.cpp
@@ -0,0 +1,182 @@
+#include <bits/stdc++.h>
+#include "linked-list.cpp"
+using namespace std;
+
+// Creating queue using Array
+/*
+class queue{
+private:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+
+public:
+    queue(int val){
+        arr = new int[val];
+        size = val;
+        front = 0;
+        rear = 0;
+    }
+
+    void push(int val){
+        if(rear == size){
+            cout << "Queue Overflow" << endl;
+        }
+        else{
+            arr[rear] = val;
+            rear++;
+        }
+    }
+
+    void pop(){
+        if(rear == front){
+            cout << "Queue Underflow" << endl;
+        }
+        else{
+            arr[front] = -1;
+            front++;
+            if(front == rear){
+                front = 0;
+                rear = 0;
+            }
+        }
+    }
+
+    int peek(){
+        if(rear == front){
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+
+    bool isEmpty(){
+        if(rear == front){
+            return true;
+        }
+        return false;
+    }
+};
+*/
+
+// Creating Circular queue using array
+/*
+class circularQueue{
+private:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+
+public:
+    circularQueue(int val){
+        arr = new int[val];
+        size = val;
+        front = -1;
+        rear = -1;
+    }
+
+    void push(int val){
+        if((front == 0 && rear == size-1) || (rear == (front-1)%(size-1))){
+            cout << "Queue Overflow" << endl;
+        }
+        else if(front == -1 && rear == -1){
+            front = rear = 0;
+            arr[rear] = val;
+        }
+        else if(rear == size-1 && front != 0){
+            rear = 0;
+            arr[rear] = val;
+        }
+        else{
+            rear++;
+            arr[rear] = val;
+        }
+    }
+
+    void pop(){
+        if(front == -1 && rear == -1){
+            cout << "Queue Underflow" << endl;
+        }
+        else if(front == rear){
+            arr[front] = -1;
+            front = rear = -1;
+        }
+        else if(front == size - 1){
+            arr[front] = -1;
+            front = 0;
+        }
+        else{
+            arr[front] = -1;
+            front++;
+        }
+    }
+
+    int peek(){
+        if(front == -1){
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+
+    bool isEmpty(){
+        if(front == -1){
+            return true;
+        }
+        return false;
+    }
+};
+*/
+
+// Creating queue using linked list
+/*
+class Queue{
+private:
+    Node* front;
+    Node* rear;
+    int size;
+
+public:
+    Queue(){
+        front = NULL;
+        rear = NULL;
+        size = 0;
+    }
+
+    void push(int val){
+        insertAtTail(front, rear, val);
+        size++;
+    }
+
+    void pop(){
+        if(front == NULL){
+            cout << "Queue Underflow" << endl;
+        }
+        else{
+            deletion(front, rear, 0);
+            size--;
+        }
+
+        if(size == 0){
+            front = rear = NULL;
+        }
+    }
+
+    int peek(){
+        if(front == NULL){
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        return front -> data;
+    }
+
+    bool isEmpty(){
+        if(front == NULL){
+            return true;
+        }
+        return false;
+    }
+};
+*/
