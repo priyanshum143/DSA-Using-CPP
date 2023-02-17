a7e6a4eba7d42645003a5847b134b1ccaf337991
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-02-17 17:02:18 +0530

    some functions added

--- Arrays.cpp
+++ Arrays.cpp
@@ -99,3 +99,99 @@ int middleEle(int arr[], int size){
     int pos = size/2;
     return arr[pos];
 }
+
+void maxTillIdx(int *arr, int len){
+    int res[len];
+    int currMax = INT8_MIN;
+
+    for(int i=0; i<len; i++){
+        if(arr[i] > currMax){
+            res[i] = arr[i];
+            currMax = arr[i];
+        }
+        else{
+            res[i] = currMax;
+        }
+    }
+
+    for(int i=0; i<len; i++){
+        cout << res[i] << " ";
+    }
+}
+
+void sumOfSubarrays(int *arr, int len){
+    for(int i=0; i<len; i++){   
+        for(int j=i; j<len; j++){
+            int sum = 0;
+            for(int k=i; k<=j; k++){
+                sum = sum + arr[k];  
+            }
+            cout << sum << " ";
+        }
+    }
+}
+
+int longestArthematicSubarray(int *arr, int len){
+    int maxLen = 2;
+    int currLen = 2;
+    int diff = arr[1] - arr[0];
+
+    for(int i=2; i<len; i++){
+        if(arr[i] - arr[i-1] == diff) currLen++;
+        else{
+            diff = arr[i] - arr[i-1];
+            currLen = 2;
+        }
+        maxLen = max(maxLen, currLen);
+    }
+    return maxLen;
+}
+
+int recordBreaker(int *arr, int len){
+    int currMax = INT16_MIN;
+    int days = 0;
+
+    for(int i=0; i<len; i++){
+        if(i == 0){
+            if(arr[i] > arr[i+1]){
+                days++;
+                currMax = arr[i];
+            }
+        } 
+        else if(i == len - 1 && currMax < arr[i]){
+            days++;
+        } 
+        else{
+            if(arr[i] > currMax){
+                days++;
+                currMax = arr[i];
+            }
+        }
+    }
+    return days;
+}
+
+int firstRepeatingEle(int *arr, int len){
+    for(int i=0; i<len; i++){
+        for(int j=i+1; j<len; j++){
+            if(arr[i] == arr[j]){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int firstMissingPositive(int *arr, int len){
+    for(int i=1; i<=len; i++){
+    bool flag = false;
+        for(int j=0; j<len; j++){
+            if(arr[j] == i){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false) return i;
+    }
+    return (len+1);
+}
