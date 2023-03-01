9df1cea7c051bdd27c3bb276490f0f7a77d5fa01
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-03-01 17:12:22 +0530

    coversions from decimal added

--- conversions.cpp
+++ conversions.cpp
@@ -7,8 +7,7 @@ int binaryToDecimal(int binNum){
 
     while(binNum > 0){
         int num = binNum % 10;
-        decNum = decNum + (pow(2, base) * num);
-        base++;
+        decNum = decNum + (pow(2, base++) * num);
         binNum = binNum / 10;
     }
     return decNum;
@@ -20,8 +19,7 @@ int octalToDecimal(int octalNum){
 
     while(octalNum > 0){
         int num = octalNum % 10;
-        decNum = decNum + (pow(8, base) * num);
-        base++;
+        decNum = decNum + (pow(8, base++) * num);
         octalNum = octalNum / 10;
     }
     return decNum;
@@ -34,12 +32,51 @@ int hexadecToDecimal(string hexadecNum){
     int len = hexadecNum.length();
     for(int i=len-1; i>=0; i--){
         if(hexadecNum[i] >= '0' && hexadecNum[i] <= '9'){
-            decNum = decNum + (pow(16, base) * (hexadecNum[i] - '0'));
+            decNum = decNum + (pow(16, base++) * (hexadecNum[i] - '0'));
         }
         else if(hexadecNum[i] >= 'A' && hexadecNum[i] <= 'F'){
-            decNum = decNum + (pow(16, base) * ((hexadecNum[i] - 'A') + 10));
+            decNum = decNum + (pow(16, base++) * ((hexadecNum[i] - 'A') + 10));
         } 
-        base++;
     }
     return decNum;
+}
+
+int decimalToBinary(int decNum){
+    int binNum = 0;
+    int base = 0;
+
+    while(decNum != 0){
+        int bit = decNum & 1;
+        binNum = binNum + (bit * pow(10, base++));
+        decNum = decNum >> 1;
+    }
+    return binNum;
+}
+
+int decimalToOctal(int decNum){
+    int octalNum = 0;
+    int base = 0;
+
+    while(decNum != 0){
+        int digit = decNum % 8;
+        octalNum = octalNum + (digit * pow(10, base++));
+        decNum = decNum / 8;
+    }
+    return octalNum;
+}
+
+string decimalToHexadec(int decNum){
+    string hexadecimal = "";
+    while(decNum != 0){
+        int digit = decNum % 16;
+        char hexaDigit;
+
+        if(digit < 10) hexaDigit = digit + '0';
+        else hexaDigit = digit + 'A' - 10;
+
+        hexadecimal = hexaDigit + hexadecimal;
+        decNum = decNum / 16;
+    }
+
+    return hexadecimal;
 }
