897b40910fe31446f1510c69874fdc7f04f9a7b0
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-04-11 23:45:30 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 void printArray(int *arr, int size){
-    for(int i=0; i<size; i++){
-        cout << arr[i] << " ";
-    }
+    for(int i=0; i<size; i++) cout << arr[i] << " ";
     cout << endl;
 }
 
@@ -36,25 +34,19 @@ int binarySearch(int arr[], int size, int targetEle){
 }
 
 void selectionSort(int arr[], int size){
-    for(int i=0; i<size; i++){
+    for(int i=0; i<size-1; i++){
+        int minIdx = i;
         for(int j=i+1; j<size; j++){
-            if(arr[i] > arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+            if(arr[j] < arr[minIdx]) minIdx = j;
         }
+        swap(arr[i], arr[minIdx]);
     }
 }
 
 void bubbleSort(int arr[], int size){
     for(int i=0; i<size-1; i++){
         for(int j=0; j<size-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp; 
-            }
+            if(arr[j] > arr[j+1]) swap(arr[j+1], arr[j]);   
         }
     }
 }
