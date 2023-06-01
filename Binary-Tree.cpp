ff00b0b8fcd97579a53341632fd398655b6da1c6
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-06-01 10:31:00 -0700

    Update Binary-Tree.cpp

--- Binary-Tree.cpp
+++ Binary-Tree.cpp
@@ -352,3 +352,19 @@ void leftView(Node* root){
         }
     }                                                                                                                                                                                                                                                                                                                             
 }
+
+// Function to flatten a binary tree into linked list without using extra space
+void flatten(Node *root){
+    Node* curr = root;
+    while(curr){
+        if(curr->left){
+            Node* pred = curr->left;
+            while(pred->right) pred = pred -> right;
+            
+            pred->right = curr->right;
+            curr->right = curr->left;
+            curr->left = NULL;
+        }
+        curr = curr->right;
+    }
+}
