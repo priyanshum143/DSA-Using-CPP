1bcb34c4908761ec01f11aedf02c0174b643ac91
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2023-06-14 04:21:14 -0700

    Update Min-Heap.cpp

--- Min-Heap.cpp
+++ Min-Heap.cpp
@@ -1,129 +1,147 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class minHeap{
+// Creating class "min-heap" using array
+class MinHeap{
 private:
-    int arr[100];
-    int size;
+    int *arr;
+    int pos;
 
 public:
-    minHeap(){
-        size = 0;
+    MinHeap(int size){
+        arr = new int[size];
+        pos = 0;
     }
 
-    void insertion(int val){
-        int idx = size;
+    void insertHeap(int val){
+        int idx = pos++;
         arr[idx] = val;
-        size++;
 
         while(idx > 0){
-            int parent = (idx-1)/2;
-            if(arr[parent] > arr[idx]){
-                swap(arr[parent], arr[idx]);
+            int parent = (idx+1)/2 - 1;
+            if(arr[idx] < arr[parent]){
+                swap(arr[idx], arr[parent]);
                 idx = parent;
             }
-            else{
-                return;
-            }
+            else return; 
         }
     }
 
-    void deletion(){
-        if(size == 0){
+    void deleteHeap(){
+        if(pos == 0){
             cout << "Heap is Empty" << endl;
             return;
         }
 
-        size--;
-        arr[0] = arr[size];
-
         int idx = 0;
-        while(idx < size){
-            int leftIndex = 2*idx + 1;
-            int rightIndex = 2*idx + 2;
+        arr[idx] = arr[--pos];
 
-            if(leftIndex < size && rightIndex < size && arr[idx] > arr[leftIndex] && arr[idx] > arr[rightIndex]){
-                if(arr[leftIndex] < arr[rightIndex]){
-                    swap(arr[idx], arr[leftIndex]);
-                    idx = leftIndex;
-                }
+        while(idx < pos){
+            int leftChild = 2*idx + 1;
+            int rightChild = 2*idx + 2;
+
+            if((leftChild < pos && arr[idx] > arr[leftChild]) && (rightChild < pos && arr[idx] > arr[rightChild])){
+                if(arr[leftChild] < arr[rightChild]){
+                    swap(arr[idx], arr[leftChild]);
+                    idx = leftChild;
+                } 
                 else{
-                    swap(arr[idx], arr[rightIndex]);
-                    idx = rightIndex;
-                }
+                    swap(arr[idx], arr[rightChild]);
+                    idx = rightChild;
+                } 
             }
-            else if(leftIndex < size && arr[idx] > arr[leftIndex]){
-                swap(arr[idx], arr[leftIndex]);
-                idx = leftIndex;
+            else if(leftChild < pos && arr[idx] > arr[leftChild]){
+                swap(arr[idx], arr[leftChild]);
+                idx = leftChild;
             }
-            else if(rightIndex < size && arr[idx] > arr[rightIndex]){
-                swap(arr[idx], arr[rightIndex]);
-                idx = rightIndex;
+            else if(rightChild < pos && arr[idx] > arr[rightChild]){
+                swap(arr[idx], arr[rightChild]);
+                idx = rightChild;
             }
-            else{
-                return;
-            }
+            else break;
         }
     }
 
     void printHeap(){
-        for(int i=0; i<size; i++){
+        for(int i=0; i<pos; i++){
             cout << arr[i] << " ";
         }
-        cout << endl;
     }
 };
 
-void heapify(int arr[], int size, int idx){
+void heapify(int *arr, int size, int idx){
     int smallest = idx;
-    int leftIdx = 2*idx + 1;
-    int rightIdx = 2*idx + 2;
+    int leftChild = 2*idx + 1;
+    int rightChild = 2*idx + 2;
 
-    if(leftIdx < size && rightIdx < size && arr[smallest] > arr[leftIdx] && arr[smallest] > arr[rightIdx]){
-        if(arr[leftIdx] < arr[rightIdx]){
-            smallest = leftIdx;
-        }
-        else{
-            smallest = rightIdx;
-        }
+    if((leftChild < size && arr[smallest] > arr[leftChild]) && (rightChild < size && arr[smallest] > arr[rightChild])){
+        if(arr[leftChild] < arr[rightChild]) smallest = leftChild;
+        else smallest = rightChild;
     }
-    else if(leftIdx < size && arr[smallest] > arr[leftIdx]){
-        smallest = leftIdx;
-    }
-    else if(rightIdx < size && arr[smallest] > arr[rightIdx]){
-        smallest = rightIdx;
-    }
+    else if(leftChild < size && arr[smallest] > arr[leftChild]) smallest = leftChild;
+    else if(rightChild < size && arr[smallest] > arr[rightChild]) smallest = rightChild;
 
     if(smallest != idx){
         swap(arr[smallest], arr[idx]);
         heapify(arr, size, smallest);
+    }     
+}
+
+void heapSort(int *arr, int size){
+    for(int i=(size/2)-1; i>=0; i--) heapify(arr, size, i);
+    while(size > 0){
+        heapify(arr, size, 0);
+        swap(arr[0], arr[--size]);
     }
 }
 
-void heapSort(int arr[], int size){
-    int n = size - 1;
-    while(n > 0){
-        swap(arr[0], arr[n]);
-        n--;
-        heapify(arr, n, 0);
+// Function to merge "k" sorted arrays
+class MergeKSortedArray{
+private:
+    struct Node{
+        int data;
+        int row;
+        int col;
+
+        Node(int data, int row, int col){
+            this->data = data;
+            this->row = row;
+            this->col = col;
+        }
+    };
+
+    class compare{
+    public:
+        bool operator()(Node* node1, Node* node2){
+            return node1->data > node2->data;
+        }
+    };
+
+public:
+    vector<int> mergeKSortedArray(vector<vector<int>> &vec, int k){
+        vector<int> ans;
+        priority_queue<Node*, vector<Node*>, compare> que;
+
+        for(int i=0; i<k; i++){
+            Node* nodeToInsert = new Node(vec[i][0], i, 0);
+            que.push(nodeToInsert);
+        }
+
+        while(!que.empty()){
+            Node* minNode = que.top();
+            que.pop();
+
+            int data = minNode->data;
+            int row = minNode->row;
+            int col = minNode->col;
+
+            ans.push_back(data);
+            if(col+1 < vec[row].size()){
+                Node* nodeToInsert = new Node(vec[row][col+1], row, col+1);
+                que.push(nodeToInsert);
+            }
+        }
+
+        return ans;
     }
-}
-
-int main(){
-    int size;
-    cin >> size;
-
-    int arr[size];
-    for(int i=0; i<size; i++) cin >> arr[i];
-
-    cout << "Building a heap => ";
-    for(int i=(size-1)/2; i>=0; i--) heapify(arr, size, i);
-    for(int i=0; i<size; i++) cout << arr[i] << " ";
-
-    cout << endl;
-
-    cout << "Sorting the given heap = > ";
-    heapSort(arr, size);
-    for(int i=0; i<size; i++) cout << arr[i] << " ";
-
-}
+};
