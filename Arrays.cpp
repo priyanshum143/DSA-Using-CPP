1b659fe64cf2eb01da23e1a35b164ea75d08d180
Priyanshu <mehtapriyanshu408@gmail.com>
2025-08-03 10:22:54 +0530

    small change

--- Arrays.cpp
+++ Arrays.cpp
@@ -43,7 +43,7 @@ void selectionSort(int arr[], int size){
 void bubbleSort(int arr[], int size){
     for(int i=0; i<size-1; i++){
         for(int j=0; j<size-i-1; j++){
-            if(arr[j] > arr[j+1]) swap(arr[j+1], arr[j]);   
+            if(arr[j] > arr[j+1]) swap(arr[j+1], arr[j]);
         }
     }
 }
